c785c6eb667b2cf72ff9fb3ac91be340f15b8de1
Lucas Piputto <lucaspiputto@gmail.com>
2021-06-12 16:12:05 -0300

    agrega funciones para hacer queries a la base de datos

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,22 @@
+const { render } = require('pug');
 const User = require('../models/User');
 
 exports.home = (req, res) => {
   User.find().sort('id').exec((err, users) => {
+    for (let user of users) {
+      user.age = Math.trunc((new Date() - user.birthday) / 31536000000);
+    }
     res.render('index', { users: users });
   });
 }
 
 exports.search = (req, res) => {
-  res.send('Implementar');
+  let result = null;
+  User.findOne({ id: req.query.id }).exec((err, user) => {
+    if (user != null) {
+      user.age = Math.trunc((new Date() - user.birthday) / 31536000000);
+      result = [user];
+    }
+    res.render('index', { users: users });
+  })
 }
